fba1e504a40aeb3185c17ed0d4e4db80d5fbe933
Jean-Philippe Tissot <jeanphilippe.tissot@gmail.com>
2020-10-19 11:22:20 -0400

    Version 0.0.3
    Include the test-runner script used by OrchardCore in package

--- rollup.config.js
+++ rollup.config.js
@@ -35,7 +35,24 @@ const config = [
           // Compile to commonjs and bundle
           commonjs()
       ],
-  }
+  },
+  {
+    input: './src/test-runner.js',
+    output: [
+        {
+            file: 'test-runner.js',
+            format: 'cjs',
+            globals: {
+                Cypress: 'cypress',
+            },
+        },
+        { file: 'test-runner.mjs', format: 'es' },
+    ],
+    plugins: [
+        // Compile to commonjs and bundle
+        commonjs()
+    ],
+}
 ];
 
 module.exports = config;
